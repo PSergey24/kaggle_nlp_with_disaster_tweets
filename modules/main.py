2659f7dbe35844e876ffca306a6c54fdb69b154f
Sergey <sproshchenko@yandex.ru>
2022-08-16 14:43:42 +0300

    refactoring

--- modules/main.py
+++ modules/main.py
@@ -25,45 +25,49 @@ class NLP:
         train_vectors = count_vectorizer.fit_transform(self.train_df["text"])
         test_vectors = count_vectorizer.transform(self.test_df["text"])
 
-        # self.train_lr(train_vectors)
-        self.train_rfc(train_vectors)
-
-        sample_submission = self.to_predict(test_vectors, model='rfc')
-        self.save_result(sample_submission, model='rfc')
-        print(1)
+        # self.train_lr(train_vectors, test_vectors)
+        self.train_rfc(train_vectors, test_vectors)
 
     @staticmethod
     def data_preprocessing(df):
         text_preprocessing = TextPreprocessing(df)
         return text_preprocessing.main()
 
-    def train_lr(self, train_vectors):
+    def train_lr(self, train_vectors, test_vectors):
         scores = model_selection.cross_val_score(self.model_lr, train_vectors, self.train_df["target"], cv=3, scoring="f1")
         print(f'LR CV scores: {scores}')
+
         self.model_lr.fit(train_vectors, self.train_df["target"])
 
-    def train_rfc(self, X_text):
+        sample_submission = self.to_predict_lr(test_vectors)
+        self.save_result(sample_submission)
+
+    def to_predict_lr(self, test_vectors):
+        sample_submission = pd.read_csv("data/sample_submission.csv")
+        sample_submission["target"] = self.model_lr.predict(test_vectors)
+        return sample_submission
+
+    def train_rfc(self, train_vectors, test_vectors):
         B = pd.concat([self.train_df[['location', 'keyword']], self.test_df[['location', 'keyword']]])
         X_cat = self.OHE.fit_transform(B)
 
         y = self.train_df['target'].to_numpy()
         X_cat = self.OHE.transform(self.train_df[['location', 'keyword']])
-        X = hstack((X_cat, X_text))
+        X = hstack((X_cat, train_vectors))
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
         self.model_rfc.fit(X_train, y_train)
         score = self.model_rfc.score(X_test, y_test)
         print(f'RFC score: {score}')
 
-    def to_predict(self, X_text, model='lr'):
-        sample_submission = pd.read_csv("data/sample_submission.csv")
+        sample_submission = self.to_predict_rfc(test_vectors)
+        self.save_result(sample_submission, model='rfc')
 
-        if model == 'lr':
-            sample_submission["target"] = self.model_lr.predict(X_text)
-        if model == 'rfc':
-            X_cat = self.OHE.transform(self.test_df[['location', 'keyword']])
-            X = hstack((X_cat, X_text))
-            sample_submission["target"] = self.model_rfc.predict(X)
+    def to_predict_rfc(self, test_vectors):
+        sample_submission = pd.read_csv("data/sample_submission.csv")
+        X_cat = self.OHE.transform(self.test_df[['location', 'keyword']])
+        X = hstack((X_cat, test_vectors))
+        sample_submission["target"] = self.model_rfc.predict(X)
         return sample_submission
 
     @staticmethod
